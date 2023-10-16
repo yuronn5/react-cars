3acec1c0dc3f7f2e080372a753ba554f2ee6b75e
yurii bilous <yurii.b@elogic.com.ua>
2023-10-16 19:53:32 +0300

    added store

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import { createRoot } from "react-dom/client";
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { Provider } from "react-redux";
+import { store } from './store'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={store}>
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    </Provider>
 );
