dedb16381a48137d8c7ce1145a91cdc2831f9c75
yurii bilous <yurii.b@elogic.com.ua>
2023-10-17 18:44:19 +0300

    added styles for project

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import CarValue from './components/CarValue';
 
 function App() {
   return (
-    <div>
+    <div className="container is-fluid">
       <CarForm />
-      <CarList />
       <CarSearch />
+      <CarList />
       <CarValue />
     </div>
   );
